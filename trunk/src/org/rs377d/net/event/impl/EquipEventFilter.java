740aa42db12b16fc06b724b49ec5dfb540dc7b93
blake@archlinux.us <blake@archlinux.us@bf12677c-f781-11de-a831-516a40b15b2e>
2010-01-03 01:41:18 +0000

    some minor readability refactoring

--- trunk/src/org/rs377d/net/event/impl/EquipEventFilter.java
+++ trunk/src/org/rs377d/net/event/impl/EquipEventFilter.java
@@ -2,6 +2,7 @@ package org.rs377d.net.event.impl;
 
 import org.apache.mina.core.session.IoSession;
 import org.rs377d.model.player.Player;
+import org.rs377d.model.util.Item;
 import org.rs377d.model.util.ItemContainer;
 import org.rs377d.net.Rs2Packet;
 import org.rs377d.net.event.EventListener;
@@ -15,11 +16,17 @@ public class EquipEventFilter implements EventListener
 	{
 		Player player = (Player) session.getAttribute("player");
 		ItemContainer inventory = (ItemContainer) player.getAttribute("inventory");
+		
 		packet.getLEShort();
 		int itemID = packet.getLEShort();
 		int slot = packet.getShortA();
-		if (inventory.get(slot).getID() != itemID)
+		
+		Item item = inventory.get(slot);
+		if (item == null)
 			return;
+		if (item.getID() != itemID)
+			return;
+		
 		packet.getPayload().rewind();
 		chain.getNext().handle(session, packet, chain);
 	}
